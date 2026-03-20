4ac0425c0a7f3f052d1acbe4184d144e7f727ae8
ernest <emushinge@gmail.com>
2026-03-20 10:01:32 -0400

    Initial Commit

---
+++ PassXYZ.Vault/App.xaml.cs
@@ -0,0 +1,37 @@
+﻿
+
+namespace PassXYZ.Vault
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            Window window = base.CreateWindow(activationState);
+            window.Created += (s, e) => {
+                Console.WriteLine("PassXYZ.Vault.App: 1. Created event");
+            };
+            window.Activated += (s, e) => {
+                Console.WriteLine("PassXYZ.Vault.App: 2. Activated event");
+            };
+            window.Deactivated += (s, e) => {
+                Console.WriteLine("PassXYZ.Vault.App: 3. Deactivated event");
+            };
+            window.Stopped += (s, e) => {
+                Console.WriteLine("PassXYZ.Vault.App: 4. Stopped event");
+            };
+            window.Resumed += (s, e) => {
+                Console.WriteLine("PassXYZ.Vault.App: 5. Resumed event");
+            };
+            window.Destroying += (s, e) => {
+                Console.WriteLine("PassXYZ.Vault.App: 6. Destroying event");
+            };
+            return new Window(new AppShell());
+        }
+    }
+
+}
