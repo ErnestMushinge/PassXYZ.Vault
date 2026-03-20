76ee0816b9d2782a301c9550cc1be56b1c4f7c3c
ernest <emushinge@gmail.com>
2026-03-20 10:30:49 -0400

    Extended the MAUI lifecycle events in another class

--- PassXYZ.Vault/App.xaml.cs
+++ PassXYZ.Vault/App.xaml.cs
@@ -10,32 +10,12 @@ namespace PassXYZ.Vault
         {
             InitializeComponent();
             //Instatieted MainPage
-            MainPage = new MainPage();
+           // MainPage = new MainPage();
         }
 
         protected override Window CreateWindow(IActivationState? activationState)
         {
-            Window window = base.CreateWindow(activationState);
-            window.Created += (s, e) => {
-                Debug.WriteLine("PassXYZ.Vault.App: 1. Created event");
-            };
-            window.Activated += (s, e) => {
-                Console.WriteLine("PassXYZ.Vault.App: 2. Activated event");
-            };
-            window.Deactivated += (s, e) => {
-                Debug.WriteLine("PassXYZ.Vault.App: 3. Deactivated event");
-            };
-            window.Stopped += (s, e) => {
-                Debug.WriteLine("PassXYZ.Vault.App: 4. Stopped event");
-            };
-            window.Resumed += (s, e) => {
-                Debug.WriteLine("PassXYZ.Vault.App: 5. Resumed event");
-            };
-            window.Destroying += (s, e) => {
-                Debug.WriteLine("PassXYZ.Vault.App: 6. Destroying event");
-            };
-            //return new Window(new AppShell());
-            return window;
+            return new PxWindow(new MainPage());
         }
     }
 
